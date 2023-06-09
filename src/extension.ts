b4946f541ffaca7b6440740fd9e7226f4582f612
ando <iamando@tuta.io>
2023-06-09 13:49:14 +0300

    feature: impl utils for source control and changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,16 @@
 import * as vscode from 'vscode'
 
+import { checkSourceControl } from './utils/source-control'
+import { checkChanges } from './utils/changes'
+
 export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "commitizen-code" is now active!'
   )
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('commitizen-code.commit', () => {
-      vscode.window.showInformationMessage('Commitizen Code!')
-    })
-  )
+  checkSourceControl({ context })
+
+  checkChanges({ context })
 }
 
 export function deactivate() {}
