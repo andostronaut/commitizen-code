220666d64f2e5e19371ddca43eee886237d63ebc
ando <iamando@tuta.io>
2023-07-15 14:50:41 +0300

    feature: use cryto instead of generated text on nonce

--- src/utils/nonce.ts
+++ src/utils/nonce.ts
@@ -1,13 +1,7 @@
+import { randomBytes } from 'crypto'
+
 function generateNonce(): string {
-  let text = ''
-  const possible =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-
-  return text
+  return randomBytes(16).toString('base64')
 }
 
 export { generateNonce }
