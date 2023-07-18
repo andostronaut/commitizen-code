5c81d64fd2b94436336c15b016ba6f18a3a58e77
ando <iamando@tuta.io>
2023-07-18 21:29:55 +0300

    feature: setup jsdoc on git util method

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,8 +1,16 @@
-function hasUntrackedGroup(untrackedGroup: Array<Object>) {
+/**
+ * @param {Array<Object>} untrackedGroup
+ * @returns {boolean}
+ */
+function hasUntrackedGroup(untrackedGroup: Array<Object>): boolean {
   return Array.isArray(untrackedGroup) && untrackedGroup.length > 0
 }
 
-function hasWorkingTreeGroup(workingTreeGroup: Array<Object>) {
+/**
+ * @param {Array<Object>} workingTreeGroup
+ * @returns {boolean}
+ */
+function hasWorkingTreeGroup(workingTreeGroup: Array<Object>): boolean {
   return Array.isArray(workingTreeGroup) && workingTreeGroup.length > 0
 }
 
