301a4b2b6649968c887eb0a6a89f86323c970458
ando <iamando@tuta.io>
2023-07-18 21:01:51 +0300

    feature: setup jsdoc on workspace util methods

--- src/utils/workspace.ts
+++ src/utils/workspace.ts
@@ -1,11 +1,19 @@
 import * as vscode from 'vscode'
 
+/**
+ * @param {vscode.WorkspaceFolder[]} workspaceFolders
+ * @returns {boolean}
+ */
 function isWorkspaceFoldersNotEmpty(
   workspaceFolders: vscode.WorkspaceFolder[]
 ): boolean {
   return workspaceFolders && workspaceFolders.length > 0
 }
 
+/**
+ * @param {vscode.WorkspaceFolder[]} workspaceFolders
+ * @returns {Promise<boolean>}
+ */
 async function isWorkspaceHasGit(
   workspaceFolders: vscode.WorkspaceFolder[]
 ): Promise<boolean> {
