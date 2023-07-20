9d664b37abc98b733d2749a4d3639fd73632e43c
ando <iamando@tuta.io>
2023-07-20 21:00:40 +0300

    feature: clean main js remove unused comment

--- media/main.js
+++ media/main.js
@@ -1,18 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-//@ts-check
-
+// @ts-nocheck
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
 ;(function () {
-  // @ts-ignore
   const vscode = acquireVsCodeApi()
   const openFolderButton = document.querySelector('#open-folder-button')
   const commitSelect = document.querySelector('#commit-select')
   const commitInput = document.querySelector('#commit-input')
   const commitButton = document.querySelector('#commit-button')
-
-  // const regexp =
-  //   /^(feature|bugfix|hotfix|chore|epic|design|experiment|documentation):(?!\s*$)\s/
 
   let commitType = 'feature'
 
@@ -21,14 +16,12 @@
   })
 
   commitSelect?.addEventListener('change', e => {
-    // @ts-ignore
     commitType = e.target.value
   })
 
   commitButton?.addEventListener('click', e => {
     e.preventDefault()
 
-    // @ts-ignore
     if (commitInput.value === '') {
       vscode.postMessage({
         type: 'error',
@@ -39,24 +32,11 @@
       return
     }
 
-    // @ts-ignore
-    // if (!commitInput.value.match(regexp)) {
-    //   vscode.postMessage({
-    //     type: 'error',
-    //     value:
-    //       'Commitizen Code: Commit message is not conform to the convention',
-    //   })
-    //   return
-    // }
-
-    // @ts-ignore
     vscode.postMessage({
       type: 'commit',
       data: {
         'commit-type': commitType,
-        // @ts-ignore
         'commit-message': commitInput.value.trim(),
-        // @ts-ignore
         commit: `${commitType}: ${commitInput.value.trim()}`,
       },
     })
