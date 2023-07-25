4c78d86c7e808d06ca7489a21b8ba45175690e82
ando <iamando@tuta.io>
2023-07-25 18:48:16 +0300

    feature: create open folder component

---
+++ src/ui/components/open-folder.ts
@@ -0,0 +1,15 @@
+/**
+ * @returns {string}
+ */
+function openFolder(): string {
+  return `
+    <div id="open-folder">
+      <span>Open Folder</span>
+      <br>
+      <br>
+      <button id="open-folder-button">Open</button>
+    </div>
+  `
+}
+
+export default openFolder
